8662a540c49bf547caa165223dd717f98739544f
melchor629 <melchor9000@gmail.com>
2017-12-08 19:00:01 +0100

    Source maps para debugging también

--- agendamlgr-cliente/config/webpack.dev.js
+++ agendamlgr-cliente/config/webpack.dev.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
 
     output: {
         path: helpers.root('web'),
